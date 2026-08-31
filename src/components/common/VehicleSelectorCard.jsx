c5f27cfc04487059cd9f11b27ae494c1fd907816
EDY28-M <junior.matias@unas.edu.pe>
2026-08-31 18:59:23 -0500

    Inicio: el selector del engranaje dice Marcas del vehículo

--- src/components/common/VehicleSelectorCard.jsx
+++ src/components/common/VehicleSelectorCard.jsx
@@ -210,7 +210,7 @@ export default function VehicleSelectorCard({ className = '' }) {
 
         <div className="parts-finder-block">
           <label htmlFor={`parts-finder-brand-${uid}`} className="parts-finder-label">
-            Marca del repuesto
+            Marcas del vehículo
           </label>
           <div className="relative">
             <select
